4983e2d3e5a98b4e16e27f25d473ba411a8c51a5
Charles Owusu <kofidesign932@gmail.com>
2019-12-01 23:29:32 +0000

    Hello

---
+++ includes/footer.php
@@ -0,0 +1,42 @@
+
+
+<footer class="dxc-footer centerit pt-2">
+  <div class="container">
+    <div class="row">
+      <div class="col-12 col-md-4">
+        <small>@ DXC Technology Company</small>
+      </div>
+
+       <div class="col-12 col-md-4  text-center">
+        <small><a href="#">Go to DXC.com</a></small>
+      </div>
+
+
+       <div class="col-12 col-md-4 text-md-right text-left">
+        <ul class="list-inline  ">
+          <li class="list-inline-item fwc subtitile"><a href="legal.php">Legal</a></li>
+          <li class="list-inline-item fwc subtitile"><a href="privacy.php">Privacy</a></li>
+          <li class="list-inline-item fwc subtitile"><a href="sitemap.php">Site Map</a><a></a></li>
+          <li class="list-inline-item fwc subtitile"><a href="optout.php">Opt Out</a></li>
+         
+        </ul>
+      </div>
+
+
+
+    </div>
+  </div>
+</footer>
+
+
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="js/jquery-3.3.1.js"></script>
+<script src="js/popper.min.js"></script>
+<script src="js/bootstrap.js"></script>
+<script src="plugins/lightbox2-master/dist/js/lightbox-plus-jquery.js"></script>
+<script src="plugins/owlcarousel/owl.carousel.min.js" ></script>
+
+<script src="js/app.js"></script>
+
+
---
+++ includes/navigation.php
@@ -0,0 +1,33 @@
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top py-3">
+    <div class="container">
+ <a href="index.php"><img src="img/dxclogo.png" class="img-fluid navbar-brand" width="250"></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+    <ul class="navbar-nav">
+      <li class="nav-item active ">
+        <a class="nav-link" href="index.php">Never miss out</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="about_us.php">Get the insights that matter</a>
+      </li>
+
+    
+
+      <li class="nav-item">
+        <a class="nav-link" href="gallery.php">See what's coming up</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="update.php">Get connected</a>
+      </li>
+       
+
+    
+    </ul>
+   
+  </div>
+  </div>
+</nav>
