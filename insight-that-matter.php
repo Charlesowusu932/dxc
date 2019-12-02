383d3de39c2fa2e59d63f3b27015f71da1663e4d
Charles Owusu <kofidesign932@gmail.com>
2019-12-02 09:41:08 +0000

    Update

--- insight-that-matter.php
+++ insight-that-matter.php
@@ -46,7 +46,7 @@
             <div class="card-body card-body3">
               <h5 class="card-title mb-3 ">Data and Digital thread</h5>
               <p class="card-text ">Is your data inhibiting your digital thread? 3 ways to establish a data strategy</p>
-             
+
             </div>
           </div>
         </div>
@@ -60,7 +60,7 @@
               <p class="card-text ">What can the A&D industry teach us about building autonomous vehicles?</p>
 
 
-             
+
             </div>
           </div>
         </div>
@@ -72,7 +72,7 @@
             <div class="card-body card-body3">
               <h5 class="card-title mb-3 ">3 CTO Takeaways</h5>
               <p class="card-text ">How digital creates an innovative workplace</p>
-             
+
             </div>
           </div>
         </div>
@@ -84,7 +84,7 @@
             <div class="card-body card-body3">
               <h5 class="card-title mb-3 ">Digital readiness</h5>
               <p class="card-text ">Digital readiness is not what you think</p>
-             
+
             </div>
           </div>
         </div>
@@ -96,67 +96,67 @@
             <div class="card-body card-body3">
               <h5 class="card-title mb-3 ">Digital strategy</h5>
               <p class="card-text ">Five ways to fix inadequate digital strategy</p>
-             
+
             </div>
           </div>
         </div>
 
 
-          <div class="col-12 col-md-6 col-lg-4 mb-3 px-2">
+        <div class="col-12 col-md-6 col-lg-4 mb-3 px-2">
           <div class="card card2 h-100">
             <img src="img/11.png" class="card-img-top" alt="...">
             <div class="card-body card-body3">
               <h5 class="card-title mb-3 ">Turn cool into value</h5>
               <p class="card-text ">Q&A: How to turn “cool” into value for the business</p>
-             
+
             </div>
           </div>
         </div>
 
 
-  <div class="col-12 col-md-6 col-lg-4 mb-3 px-2">
+        <div class="col-12 col-md-6 col-lg-4 mb-3 px-2">
           <div class="card card2 h-100">
             <img src="img/12.png" class="card-img-top" alt="...">
             <div class="card-body card-body3">
               <h5 class="card-title mb-3 ">The first full Cyborg</h5>
               <p class="card-text ">The work-a-thon that helps Dr. Peter Scott-Morgan become a cyborg, or Peter 2.0</p>
-             
+
             </div>
           </div>
         </div>
 
 
-  <div class="col-12 col-md-6 col-lg-4 mb-3 px-2">
+        <div class="col-12 col-md-6 col-lg-4 mb-3 px-2">
           <div class="card card2 h-100">
             <img src="img/13.png" class="card-img-top" alt="...">
             <div class="card-body card-body3">
               <h5 class="card-title mb-3 ">Autonomous ethics</h5>
               <p class="card-text ">Tackling ethical roadblocks in AI and autonomous driving</p>
-             
+
             </div>
           </div>
         </div>
 
 
-  <div class="col-12 col-md-6 col-lg-4 mb-3 px-2">
+        <div class="col-12 col-md-6 col-lg-4 mb-3 px-2">
           <div class="card card2 h-100">
             <img src="img/14.png" class="card-img-top" alt="...">
             <div class="card-body card-body3">
               <h5 class="card-title mb-3 ">Research Insights</h5>
               <p class="card-text ">Get exclusive access to LEF’s research and commentaries that reveal emerging trends…</p>
-             
+
             </div>
           </div>
         </div>
 
 
-  <div class="col-12 col-md-6 col-lg-4 mb-3 px-2">
+        <div class="col-12 col-md-6 col-lg-4 mb-3 px-2">
           <div class="card card2 h-100">
             <img src="img/15.png" class="card-img-top" alt="...">
             <div class="card-body card-body3">
               <h5 class="card-title mb-3 ">A Tale of Two Missions</h5>
               <p class="card-text ">FWhat should companies be doing to make the most of the dramatic new level of value creation</p>
-             
+
             </div>
           </div>
         </div>
@@ -164,7 +164,7 @@
 
 
 
-       
+
 
 
       </div>
@@ -183,8 +183,8 @@
 
         <div class="col-12 col-md-6  px-2 mb-3">
           <div class="thrive">
-          <h3 class="font-weight-bold">Thrive Q&A</h3>
-          <p><small>Don’t miss out on our forthcoming events calendar.</small></p>
+            <h3 class="font-weight-bold">Thrive Q&A</h3>
+            <p><small>Don’t miss out on our forthcoming events calendar.</small></p>
 
             <a href="#" class="btn btn btn-warning mt-5">FIND OUT MORE</a>
           </div>
@@ -193,8 +193,8 @@
 
         <div class="col-12 col-md-6  px-2 mb-3">
           <div class="thrive">
-          <h3 class="font-weight-bold">Your pathway to digital success</h3>
-          <p><small>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis consectetur adipisicing.</small></p>
+            <h3 class="font-weight-bold">Your pathway to digital success</h3>
+            <p><small>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis consectetur adipisicing.</small></p>
 
             <a href="#" class="btn btn btn-warning mt-5">FIND OUT MORE</a>
           </div>
@@ -207,77 +207,104 @@
 
 
   <!-- be inspired -->
-    <section class="bismain ">
-      <div class="container">
-        <div class="row">
-            <div class="col-10 mx-auto">
-              <div class="row centerit">
-          <div class="col-12 col-md-6">
-           <div class="bline">
-                <h3>Be Inspired.</h3>
+  <section class="bismain ">
+    <div class="container">
+      <div class="row">
+        <div class="col-10 mx-auto">
+          <div class="row centerit">
+            <div class="col-12 col-md-6">
+             <div class="bline">
+              <h3>Be Inspired.</h3>
               <p><small>Introducing Thrive, your daily source of compelling content on the power of change.</small></p>
 
               <a href="#" class="btn btn-primary2 btn-block">SUBSCRIBE TO THRIVE NEWSLETTER</a>
 
               <a href="#" class="btn btn-primary1 btn-block">VISIT THRIVE</a>
-           </div>
-          </div>
-
-          <div class="col-12 col-md-6 thrive">
-             <h3 class="mb-4">Join the conversation</h3>
-            <p class="my-4"><small>You are not alone on your digital transformation journey. Join <br>DXC Technology’s Thrive LinkeIn Group, a community centred one embracing the power of innovation to thrive o change</small></p>
-
-             <a href="#" class="btn btn-primary2 ">LINKEDIN GROUP</a>
-          </div>
-        </div>
             </div>
-         </div>
-      </div>
-    </section>
-  <!-- be inspired -->
+          </div>
 
+          <div class="col-12 col-md-6 thrive moveleft">
+           <h3 class="mb-4">Join the conversation</h3>
+           <p class="my-4"><small>You are not alone on your digital transformation journey. Join <br>DXC Technology’s Thrive LinkeIn Group, a community centred one embracing the power of innovation to thrive o change</small></p>
 
+           <a href="#" class="btn btn-primary2 mb-4">LINKEDIN GROUP</a>
 
-
-
-
-  <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalCenterTitle">Modal title</h5>
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-          </button>
-        </div>
-        <div class="modal-body">
-          ...
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-          <button type="button" class="btn btn-primary">Save changes</button>
+           <div class="row">
+              <div class="col-3 pr-0">
+                <small>Follow Us</small>
+              </div>
+              <div class="col-9 pl-0">
+              <ul class="list-inline">
+              <li class="list-inline-item  fwc subtitile"><a href="#"><i class="fa fa-2xrr fa-facebook-official" aria-hidden="true"></i></a></li>
+              <li class="list-inline-item  fwc subtitile"><a href="#"><i class="fa fa-2xrr fa-twitter" aria-hidden="true"></i></a></li>
+              <li class="list-inline-item  fwc subtitile"><a href="#"><i class="fa fa-2xrr fa-linkedin" aria-hidden="true"></i></a></li>
+              <li class="list-inline-item  fwc subtitile"><a href="#"><i class="fa fa-2xrr fa-youtube-play" aria-hidden="true"></i></a></li>
+              <li class="list-inline-item  fwc subtitile"><a href="#"><i class="fa fa-2xrr fa-rss" aria-hidden="true"></i></a></li>
+              
+            </ul>
+              </div>
+          </div>
         </div>
       </div>
     </div>
   </div>
+</div>
+</section>
+<!-- be inspired -->
 
-  <div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-lg modal-dialog-centered">
-      <div class="modal-content modal-content1">
-        <div class="modal-header modal-header1">
-
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-          </button>
-        </div>
-        <div class="modal-body">
-         <div class="embed-responsive embed-responsive-16by9">
-          <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/8XE0pol9R50" allowfullscreen></iframe>
+<!-- brag insight -->
+  <section>
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <img src="img/getinspired.png" alt="" class="img-fluid">
         </div>
       </div>
+    </div> 
+  </section>
+<!-- brag insight -->
 
+
+
+
+
+<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalCenterTitle">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
     </div>
   </div>
+</div>
+
+<div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-lg modal-dialog-centered">
+    <div class="modal-content modal-content1">
+      <div class="modal-header modal-header1">
+
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+       <div class="embed-responsive embed-responsive-16by9">
+        <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/8XE0pol9R50" allowfullscreen></iframe>
+      </div>
+    </div>
+
+  </div>
+</div>
 </div>
 </div>
 
