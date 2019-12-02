e7137b8b5b102b887a69ee1b5e685a1b5418e6a0
Charles Owusu <kofidesign932@gmail.com>
2019-12-02 19:12:29 +0000

    UPDATE

--- index.php
+++ index.php
@@ -18,25 +18,40 @@
 
 
 
-<section class="entercode centerit">
-  <div class="container">
-    <div class="row">
-      <div class="col-12 text-center mb-4">
-        <p class="wc">Enter your code to access exclusive <br> DXC services for VIP customers.</p>
+  <section class="entercode centerit">
+    <div class="container">
+      <div class="row">
+        <div class="col-12 text-center mb-4">
+          <p class="wc">Enter your code to access exclusive <br> DXC services for VIP customers.</p>
+        </div>
+
+
+        <div class="col-12 text-center">
+          <!-- <div class="form-group">
+            <input type="number" placeholder=""  class="form-control autonumber">
+            <span class="font-14 text-muted">e.g. "9,999"</span>
+
+            <input type="number" min="1" max="1" step="1" value="1">
+          </div> -->
+          <h1 class="lbk">000-000-000</h1>
+        </div>
+
+
+        <div class="col-12 text-center fixed-bottom pb-5">
+          <a href="#"><small class="lbk">Forgot your code ?</small></a>
+        </div>
       </div>
     </div>
-  </div>
-</section>
+  </section>
 
 
 
 
 
 
-
-<!-- footer includes -->
-<?php require_once("includes/footer2.php");?>
-<!-- footer includes -->
+  <!-- footer includes -->
+  <?php require_once("includes/footer2.php");?>
+  <!-- footer includes -->
 
 </body>
 </html>
