e81866e49f70aff5e913bdb8ec2dd45472501409
Charles Owusu <kofidesign932@gmail.com>
2019-12-03 05:53:11 +0000

    Uodate

--- welcome.php
+++ welcome.php
@@ -20,39 +20,39 @@
 
   <section class="welcome centerit">
     <div class="container">
-      <div class="row ">
-      
-     <!--   <div class="col-12 text-center">
-            <img src="img/roundblack.png" class="img-fluid w-50" alt="">
-       </div> -->
-
-        <div class="col-10 col-md-6 mx-auto  text-center ">
-
-
-           <h4 class="font-weight-bold wc mb-4">  Hi < Name >  </h4>
-<p>Thank you for joining us at the <strong class="wc">World Business Forum.</strong>  With <br> digital redefining the [industry], we hope the  <br>event inspired new thinking, and will enable <strong class="wc"> [business <br> name]</strong> to embrace the power of change </p>
-
-<h4 class="font-weight-bold wc mb-4">Lets keep the conversation going<span class="dxc-yellow">.</span></h4>
-
-<p>We know how precious your time is, So we’ve created a <br> platform that’ll keep you at the bleeding edge. Of innovation.</p>
-
-<p class="font-weight-bold wc">All the insight you need, all I one place</p>
-
-<h4 class="font-weight-bold wc">Welcome to the <br>Executive Lounge <span class="xc-yellow">.</span></h4>
-        </div>
-
+      <div class="row containerm">
+        
+       <div class="col-12 text-center">
+        <img src="img/roundblack.png" class="img-fluid w-50  " alt="">
       </div>
-    </div>
-  </section>
+
+      <div class="col-10 col-md-6 mx-auto  text-center centered-element">
+
+
+       <h4 class="font-weight-bold wc mb-4">  Hi < Name >  </h4>
+       <p>Thank you for joining us at the <strong class="wc">World Business Forum.</strong>  With <br> digital redefining the [industry], we hope the  <br>event inspired new thinking, and will enable <strong class="wc"> [business <br> name]</strong> to embrace the power of change </p>
+
+       <h4 class="font-weight-bold wc mb-4">Lets keep the conversation going<span class="dxc-yellow">.</span></h4>
+
+       <p>We know how precious your time is, So we’ve created a <br> platform that’ll keep you at the bleeding edge. Of innovation.</p>
+
+       <p class="font-weight-bold wc">All the insight you need, all I one place</p>
+
+       <h4 class="font-weight-bold wc">Welcome to the <br>Executive Lounge<span class="xc-yellow">.</span></h4>
+     </div>
+
+   </div>
+ </div>
+</section>
 
 
 
 
 
 
-  <!-- footer includes -->
-  <?php require_once("includes/footer2.php");?>
-  <!-- footer includes -->
+<!-- footer includes -->
+<?php require_once("includes/footer2.php");?>
+<!-- footer includes -->
 
 </body>
 </html>
