fb27547aba6d5a16d497bc0391afbc409b0ef1c0
Charles Owusu <kofidesign932@gmail.com>
2019-12-03 05:40:15 +0000

    UPDATE

---
+++ welcome.php
@@ -0,0 +1,58 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <title>DXC Technology | Welcome Splash</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="keywords" content="">
+  <meta name="author" content="">
+
+
+
+  <!-- include navigation -->
+  <?php require_once("includes/nav-links.php");?>
+  <?php require_once("includes/navigation2.php");?>
+  <!-- include navigation -->
+
+
+
+
+  <section class="welcome centerit">
+    <div class="container">
+      <div class="row ">
+      
+     <!--   <div class="col-12 text-center">
+            <img src="img/roundblack.png" class="img-fluid w-50" alt="">
+       </div> -->
+
+        <div class="col-10 col-md-6 mx-auto  text-center ">
+
+
+           <h4 class="font-weight-bold wc mb-4">  Hi < Name >  </h4>
+<p>Thank you for joining us at the <strong class="wc">World Business Forum.</strong>  With <br> digital redefining the [industry], we hope the  <br>event inspired new thinking, and will enable <strong class="wc"> [business <br> name]</strong> to embrace the power of change </p>
+
+<h4 class="font-weight-bold wc mb-4">Lets keep the conversation going<span class="dxc-yellow">.</span></h4>
+
+<p>We know how precious your time is, So we’ve created a <br> platform that’ll keep you at the bleeding edge. Of innovation.</p>
+
+<p class="font-weight-bold wc">All the insight you need, all I one place</p>
+
+<h4 class="font-weight-bold wc">Welcome to the <br>Executive Lounge <span class="xc-yellow">.</span></h4>
+        </div>
+
+      </div>
+    </div>
+  </section>
+
+
+
+
+
+
+  <!-- footer includes -->
+  <?php require_once("includes/footer2.php");?>
+  <!-- footer includes -->
+
+</body>
+</html>
