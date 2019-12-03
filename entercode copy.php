7faa1a43df690a6e98d91f6092e7e956ed7b4341
Charles Owusu <kofidesign932@gmail.com>
2019-12-03 04:49:26 +0000

    Welcome

---
+++ entercode copy.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <title>DXC Technology | Enter Code</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="keywords" content="">
+  <meta name="author" content="">
+
+
+
+  <!-- include navigation -->
+  <?php require_once("includes/nav-links.php");?>
+  <?php require_once("includes/navigation2.php");?>
+  <!-- include navigation -->
+
+
+
+
+  <section class="entercode centerit">
+    <div class="container">
+      <div class="row">
+      
+
+
+        <div class="col-12 text-center">
+        <a href="welcome.php">
+       <img src="img/circles.png" class="img-fluid" width="600"  alt="">
+       </a>
+        </div>
+
+      </div>
+    </div>
+  </section>
+
+
+
+
+
+
+  <!-- footer includes -->
+  <?php require_once("includes/footer2.php");?>
+  <!-- footer includes -->
+
+</body>
+</html>
