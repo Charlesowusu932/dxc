85af6a1eee5bf9445944cf981fbb56b80fbb6433
Charles Owusu <kofidesign932@gmail.com>
2019-12-03 06:14:29 +0000

    Update Page Names

--- entercode.php
+++ entercode.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html lang="en">
 <head>
-  <title>DXC Technology | Enter Code Go</title>
+  <title>DXC Technology | Enter Code</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta name="description" content="">
@@ -21,15 +21,25 @@
   <section class="entercode centerit">
     <div class="container">
       <div class="row">
-      
+        <div class="col-12 text-center mb-4">
+          <p class="wc">Enter your code to access exclusive <br> DXC services for VIP customers.</p>
+        </div>
 
 
         <div class="col-12 text-center">
-        <a href="welcome.php">
-       <img src="img/circles.png" class="img-fluid" width="600"  alt="">
-       </a>
+          <!-- <div class="form-group">
+            <input type="number" placeholder=""  class="form-control autonumber">
+            <span class="font-14 text-muted">e.g. "9,999"</span>
+
+            <input type="number" min="1" max="1" step="1" value="1">
+          </div> -->
+          <h1 class="lbk">000-000-000</h1>
         </div>
 
+
+        <div class="col-12 text-center fixed-bottom pb-5">
+          <a href="#"><small class="lbk">Forgot your code ?</small></a>
+        </div>
       </div>
     </div>
   </section>
